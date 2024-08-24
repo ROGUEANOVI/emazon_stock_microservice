6677225bd646776a6ffc2d38c86dc979811ada41
rogueanovi <ovidioromero66@gmail.com>
2024-08-23 21:37:44 -0500

    first commit

---
+++ src/main/java/com/pragma/emazon/stock_microservice/StockMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pragma.emazon.stock_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMicroserviceApplication.class, args);
+	}
+
+}
